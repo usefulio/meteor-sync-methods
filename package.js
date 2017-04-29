e6c48d6868f5d4589e572d86e26f1632b47011a5
Recursive Lambda God <chris@circle.academy>
2017-04-29 14:16:25 -0400

    "Recursive Lambda Refactor V1"

--- package.js
+++ package.js
@@ -7,14 +7,14 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(api => {
   api.versionsFrom('1.3.2.4');
   api.use('ecmascript');
   api.use('underscore', 'server')
   api.mainModule('sync-new.js', 'server');
 });
 
-Package.onTest(function(api) {
+Package.onTest(api => {
   api.use('ecmascript');
   api.use('tinytest');
   // api.use('sync-new');
